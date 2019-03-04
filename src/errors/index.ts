ce930733201c0712bbfd014e846d5d96abce8d7c
Mariocoski <mariuszrajczakowski@gmail.com>
2019-03-04 21:39:06 +0000

    fix(export): added package export

---
+++ src/errors/index.ts
@@ -0,0 +1,7 @@
+import conflictingItemError from "./conflictingItemError";
+import itemNotFoundError from "./itemNotFoundError";
+
+export {
+  conflictingItemError,
+  itemNotFoundError
+};
---
+++ src/functions/index.ts
@@ -0,0 +1,19 @@
+import countItems from "./countItems";
+import createItem from "./createItem";
+import deleteItem from "./deleteItem";
+import deleteItems from "./deleteItems";
+import getItem from "./getItem";
+import getItems from "./getItems";
+import replaceItem from "./replaceItem";
+import updateItem from "./updateItem";
+
+export {
+  countItems,
+  createItem,
+  getItems,
+  getItem,
+  deleteItem,
+  deleteItems,
+  replaceItem,
+  updateItem
+};
