a7bb94d576e7bb2810b332e28838a7fcbb3dace7
Mariocoski <mariuszrajczakowski@gmail.com>
2019-03-05 00:37:57 +0000

    fix: typings fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,6 @@
-export {};
+export * from "./errors";
+export * from "./facade";
+export * from "./functions";
+export * from "./interfaces";
+export * from "./testFacade";
+export * from "./utils";
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import * as errors from "./errors/index";
-import * as facade from "./facade";
-import * as functions from "./functions";
-import * as interfaces from "./interfaces";
-import * as testFacade from "./testFacade";
-import * as utils from "./utils";
-
-module.exports = {
-  ...functions,
-  ...errors,
-  ...interfaces,
-  ...utils,
-  facade,
-  testFacade
-};
+export * from "./errors/index";
+export * from "./facade";
+export * from "./functions";
+export * from "./interfaces";
+export * from "./testFacade";
+export * from "./utils";
